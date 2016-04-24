1b83df2b2cc57bc880462f316b4cc49a6821074d
Hidekazu Furuta <furuta@mxb.mesh.ne.jp>
2016-04-24 23:26:01 +0900

    たっぷりデジカメを見るためのViewerを追加

---
+++ PhotoView/PhotoView/FormPhotoView.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PhotoView
+{
+    partial class FormPhotoView
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listBoxRoot = new System.Windows.Forms.ListBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.listBoxCategory = new System.Windows.Forms.ListBox();
+            this.listBoxAlbum = new System.Windows.Forms.ListBox();
+            this.explorerBrowserContents = new Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.explorerBrowserContents);
+            this.splitContainer1.Size = new System.Drawing.Size(910, 529);
+            this.splitContainer1.SplitterDistance = 303;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listBoxRoot);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(303, 529);
+            this.splitContainer2.SplitterDistance = 81;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // listBoxRoot
+            // 
+            this.listBoxRoot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxRoot.FormattingEnabled = true;
+            this.listBoxRoot.ItemHeight = 15;
+            this.listBoxRoot.Location = new System.Drawing.Point(0, 0);
+            this.listBoxRoot.Name = "listBoxRoot";
+            this.listBoxRoot.Size = new System.Drawing.Size(303, 81);
+            this.listBoxRoot.TabIndex = 0;
+            this.listBoxRoot.SelectedIndexChanged += new System.EventHandler(this.listBoxRoot_SelectedIndexChanged);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.listBoxCategory);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.listBoxAlbum);
+            this.splitContainer3.Size = new System.Drawing.Size(303, 444);
+            this.splitContainer3.SplitterDistance = 124;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // listBoxCategory
+            // 
+            this.listBoxCategory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxCategory.FormattingEnabled = true;
+            this.listBoxCategory.ItemHeight = 15;
+            this.listBoxCategory.Location = new System.Drawing.Point(0, 0);
+            this.listBoxCategory.Name = "listBoxCategory";
+            this.listBoxCategory.Size = new System.Drawing.Size(303, 124);
+            this.listBoxCategory.TabIndex = 0;
+            this.listBoxCategory.SelectedIndexChanged += new System.EventHandler(this.listBoxCategory_SelectedIndexChanged);
+            // 
+            // listBoxAlbum
+            // 
+            this.listBoxAlbum.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxAlbum.FormattingEnabled = true;
+            this.listBoxAlbum.ItemHeight = 15;
+            this.listBoxAlbum.Location = new System.Drawing.Point(0, 0);
+            this.listBoxAlbum.Name = "listBoxAlbum";
+            this.listBoxAlbum.Size = new System.Drawing.Size(303, 316);
+            this.listBoxAlbum.TabIndex = 0;
+            this.listBoxAlbum.SelectedIndexChanged += new System.EventHandler(this.listBoxAlbum_SelectedIndexChanged);
+            // 
+            // explorerBrowserContents
+            // 
+            this.explorerBrowserContents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.explorerBrowserContents.Location = new System.Drawing.Point(0, 0);
+            this.explorerBrowserContents.Name = "explorerBrowserContents";
+            this.explorerBrowserContents.PropertyBagName = "Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser";
+            this.explorerBrowserContents.Size = new System.Drawing.Size(603, 529);
+            this.explorerBrowserContents.TabIndex = 0;
+            // 
+            // FormPhotoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 529);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormPhotoView";
+            this.Text = "PhotoView";
+            this.Load += new System.EventHandler(this.FormPhotoView_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListBox listBoxRoot;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.ListBox listBoxCategory;
+        private System.Windows.Forms.ListBox listBoxAlbum;
+        private Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser explorerBrowserContents;
+    }
+}
+
---
+++ PhotoView/PhotoView/FormPhotoView.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Controls;
+
+namespace PhotoView
+{
+    public partial class FormPhotoView : Form
+    {
+        public FormPhotoView()
+        {
+            InitializeComponent();
+            initRootListBox();
+        }
+
+        private IEnumerable<Tuple<int, string>> profilesToIdsAndTitles(IEnumerable<string> profiles)
+        {
+            string idtbl = GetPrivateProfileString(profiles, "DATA", "IDTBL", "失敗");
+            if ( idtbl != "失敗" && idtbl != "" )
+            {
+                string[] nums = idtbl.Split(new char[] { ',' });
+                foreach ( string strNum in nums )
+                {
+                    int n = Int32.Parse(strNum);
+                    string id = "ID" + n.ToString("d5");
+                    string title = GetPrivateProfileString(profiles, id, "TITLE", "失敗");
+                    if ( title != "失敗" )
+                    {
+                        yield return new Tuple<int, string>(n, title);
+                    }
+                }
+            }
+        }
+
+        private void addToListBox(ListBox lb, IEnumerable<Tuple<int, string>> items)
+        {
+            lb.Items.Clear();
+            foreach ( Tuple<int, string> item in items )
+            {
+                lb.Items.Add(item.Item2);
+            }
+        }
+
+        private string GetPrivateProfileString(IEnumerable<string> profiles, string section, string key, string defaultValue)
+        {
+            string currentSection = "";
+            string currentKey = "";
+            foreach ( string line in profiles )
+            {
+                if ( Regex.IsMatch(line, @"^\[.*\]$") )
+                {
+                    currentSection = Regex.Match(line, @"^\[(.*)\]$").Groups[1].Value;
+                }
+                else if ( Regex.IsMatch(line, @"^.*\=.*$") )
+                {
+                    currentKey = Regex.Match(line, @"^(.*)\=(.*)$").Groups[1].Value;
+                    if ( currentSection == section && currentKey == key )
+                    {
+                        return Regex.Match(line, @"^(.*)\=(.*)$").Groups[2].Value;
+                    }
+                }
+            }
+            return defaultValue;
+        }
+
+        private IEnumerable<string> readLines(string fileName)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(fileName, Encoding.Default);
+
+                List<string> lines = new List<string>();
+                while ( !sr.EndOfStream )
+                {
+                    lines.Add(sr.ReadLine());
+                }
+
+                sr.Close();
+
+                return lines;
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"J:\furuta\photo4\たっぷりデジカメ";
+            openFileDialog1.Filter = "ini files (*.ini)|*.ini|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if ( openFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                IEnumerable<string> profiles = readLines(openFileDialog1.FileName);
+                categoryFolder = Path.GetDirectoryName(openFileDialog1.FileName);
+                categories = profilesToIdsAndTitles(profiles);
+                addToListBox(listBoxCategory, categories);
+            }
+        }
+
+        private List<string> rootFolders;
+
+        private void initRootListBox()
+        {
+            rootFolders = new List<string>();
+            rootFolders.Add(@"J:\furuta\photo");
+            rootFolders.Add(@"J:\furuta\photo2");
+            rootFolders.Add(@"J:\furuta\photo3");
+            rootFolders.Add(@"J:\furuta\photo4");
+            foreach ( string folder in rootFolders )
+            {
+                listBoxRoot.Items.Add(folder);
+            }
+        }
+
+        private void listBoxRoot_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBoxRoot.SelectedIndex;
+            // タイミングによって index が -1 になるのを回避
+            if ( index >= 0 )
+            {
+                string inifile = rootFolders[index] + @"\たっぷりデジカメ\__bookbox.ini";
+                IEnumerable<string> profiles = readLines(inifile);
+                categoryFolder = Path.GetDirectoryName(inifile);
+                categories = profilesToIdsAndTitles(profiles);
+                addToListBox(listBoxCategory, categories);
+            }
+        }
+
+        private string categoryFolder = "";
+        private IEnumerable<Tuple<int, string>> categories = null;
+
+        private void listBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBoxCategory.SelectedIndex;
+            // タイミングによって index が -1 になるのを回避
+            if ( index >= 0 )
+            {
+                if ( categories != null )
+                {
+                    int id = categories.ElementAt(index).Item1;
+                    string inifile = categoryFolder + @"\a" + id.ToString("d5") + @"\__bookbox.ini";
+                    albumFolder = Path.GetDirectoryName(inifile);
+                    IEnumerable<string> lines = readLines(inifile);
+                    if ( lines != null )
+                    {
+                        albums = profilesToIdsAndTitles(lines);
+                        addToListBox(listBoxAlbum, albums);
+                    }
+                }
+            }
+        }
+
+        private string albumFolder = "";
+        private IEnumerable<Tuple<int, string>> albums = null;
+
+        private void listBoxAlbum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBoxAlbum.SelectedIndex;
+            // タイミングによって index が -1 になるのを回避
+            if ( index >= 0 )
+            {
+                if ( albums != null )
+                {
+                    int id = albums.ElementAt(index).Item1;
+                    string photoFolder = albumFolder + @"\a" + id.ToString("d5") + @"\DCIM\100FFIMG";
+                    explorerBrowserContents.Navigate(ShellObject.FromParsingName(photoFolder));
+                }
+            }
+        }
+
+        private void FormPhotoView_Load(object sender, EventArgs e)
+        {
+            explorerBrowserContents.NavigationOptions.PaneVisibility.Navigation = PaneVisibilityState.Hide;
+            explorerBrowserContents.Navigate((ShellObject)KnownFolders.Desktop);
+        }
+    }
+}
